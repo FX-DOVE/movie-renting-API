5fb6ef96aec819b11061b87c87506d9cbf5c6a19
fx-dove <opiechisomsamuel@gmail.com>
2024-01-27 18:22:43 +0100

    second commit

--- number3querry.js
+++ number3querry.js
@@ -13,7 +13,8 @@ class MovieStore {
     addMovie(movie) {
         this.movies.push(movie);
     }
-
+   
+//function that rent movies
     rentMovie(title) {
         let movie = this.movies.find(movie => movie.title === title);
         if (movie && movie.availableCopies > 0) {
@@ -23,27 +24,28 @@ class MovieStore {
             return `${title} is currently unavailable`;
         }
     }
-
+// function that returns movie
     returnMovie(title) {
         let movie = this.movies.find(movie => movie.title === title);
         if (movie) {
             movie.availableCopies++;
             return `You have successfully returned ${title}`;
         } else {
-            return `We do not have ${title} in our records`;
+            return `you did not rent ${title} `;
         }
     }
 }
 
-// Usage
+// creating movies stores
 let store = new MovieStore();
 store.addMovie(new Movie('Inception', 3));
 store.addMovie(new Movie('Interstellar', 2));
 store.addMovie(new Movie('merlin', 0));
 store.addMovie(new Movie('ben 10', 8));
 
-console.log(store.rentMovie('Inception'));  // You have successfully rented Inception
-console.log(store.rentMovie('chisom'));  // You have successfully rented Inception
-console.log(store.rentMovie('Inception'));  // You have successfully rented Inception
-console.log(store.rentMovie('Inception'));  // Inception is currently unavailable
+
+console.log(store.findMovie('title')) // is used to check if the movies you want is in the app 
+console.log(store.rentMovie('title'));  // is used to rent movie 
+console.log(store.returnMovie('title'));  // is used to return movie after watching
+
 
